a1bd5300d6a5d534af1456f92f391c166b8f6c8a
Viet Huynh <xa.lach@yahoo.com>
2024-04-05 06:24:41 +0700

    add unit tests

--- client/src/app/ticket-details/ticket-details.spec.tsx
+++ client/src/app/ticket-details/ticket-details.spec.tsx
@@ -1,10 +1,25 @@
-import { render } from '@testing-library/react';
-
+import { render, screen } from '@testing-library/react';
 import TicketDetails from './ticket-details';
+import useGetTicket from '../../hooks/useGetTicket';
+import useGetUser from '../../hooks/useGetUser';
 
-describe('TicketDetails', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<TicketDetails />);
-    expect(baseElement).toBeTruthy();
+jest.mock('../../hooks/useGetTicket', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+jest.mock('../../hooks/useGetUser', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+describe(TicketDetails.name, () => {
+  it('should render Ticket not found message when one is not found', () => {
+    (useGetTicket as jest.Mock).mockReturnValue({});
+    (useGetUser as jest.Mock).mockReturnValue({});
+
+    render(<TicketDetails />);
+
+    expect(screen.getByText('Ticket not found.')).toBeInTheDocument();
   });
 });
--- client/src/app/tickets/tickets.spec.tsx
+++ client/src/app/tickets/tickets.spec.tsx
@@ -1,10 +1,60 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import Tickets from './tickets';
+import useGetTickets from '../../hooks/useGetTickets';
+import useGetUsers from '../../hooks/useGetUsers';
 
-describe('Tickets', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Tickets />);
-    expect(baseElement).toBeTruthy();
+jest.mock('../../hooks/useGetTickets', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+jest.mock('../../hooks/useGetUsers', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+describe(Tickets.name, () => {
+  it('should render error message when there is any', () => {
+    (useGetTickets as jest.Mock).mockReturnValue({
+      error: new Error('test error'),
+    });
+    (useGetUsers as jest.Mock).mockReturnValue({});
+
+    render(<Tickets />);
+
+    expect(screen.getByText('test error')).toBeInTheDocument();
+  });
+
+  it('should render spinner when data is still being loaded', () => {
+    (useGetTickets as jest.Mock).mockReturnValue({ isLoading: true });
+    (useGetUsers as jest.Mock).mockReturnValue({});
+
+    render(<Tickets />);
+
+    expect(screen.getByRole('progressbar')).toBeInTheDocument();
+  });
+
+  it('should render tickets when they are loaded', () => {
+    (useGetTickets as jest.Mock).mockReturnValue({
+      data: [
+        {
+          id: 1,
+          description: 'Description',
+          completed: false,
+          assigneeId: 1,
+        },
+      ],
+    });
+    (useGetUsers as jest.Mock).mockReturnValue({
+      data: [{ id: 1, name: 'User' }],
+    });
+
+    render(<Tickets />, { wrapper: MemoryRouter });
+
+    expect(
+      screen.getByText('Ticket 1: Description - User')
+    ).toBeInTheDocument();
   });
 });
